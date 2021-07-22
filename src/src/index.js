e1bc11059a5b0aec50465bfd1ef1b231fb6cc190
moka <gaoyuanfell@sina.com>
2021-07-22 22:47:03 +0800

    2021-07-22 22:47:03 → moka:gaoyuanfell@sina.com | msg →

--- src/src/index.js
+++ src/src/index.js
@@ -2,27 +2,32 @@ const router = [
   {
     path: "home",
     name: "home",
-    component: "home"
+    component: "home",
+    loaded: false
   },
   {
     path: "product",
     name: "product",
-    component: "product"
+    component: "product",
+    loaded: false
   },
   {
     path: "store",
     name: "store",
-    component: "store"
+    component: "store",
+    loaded: false
   },
   {
     path: "about",
     name: "about",
-    component: "about"
+    component: "about",
+    loaded: false
   },
   {
     path: "join",
     name: "join",
-    component: "join"
+    component: "join",
+    loaded: false
   }
 ];
 
@@ -40,32 +45,53 @@ function animate(ref) {
 
 function hashchange(hash = "home") {
   for (let index = 0; index < router.length; index++) {
-    const { path, component } = router[index];
+    const { component } = router[index];
     const ref = document.querySelector(`[route-id="${component}"]`);
     const link = document.querySelector(`[route-link="${component}"]`);
     if (!ref) continue;
-    if (path !== hash) {
-      ref.style.display = "none";
-      link && link.classList.remove("action");
-      continue;
-    }
+    ref.style.display = "none";
+    link && link.classList.remove("action");
+  }
 
-    link && link.classList.add("action");
-    if (ref.nodeName === "TEMPLATE") {
-      const clon = ref.content.cloneNode(true);
-      ref.parentNode.insertBefore(clon, ref);
-      ref.parentNode.removeChild(ref);
-      const _ref = document.querySelector(`[route-id="${component}"]`);
-      animate(_ref);
-    } else {
-      ref.style.display = "";
-      animate(ref);
-    }
+  const route = router.find((r) => r.path === hash);
+  if (!route) return;
+  const ref = document.querySelector(`[route-id="${route.component}"]`);
+  const link = document.querySelector(`[route-link="${route.component}"]`);
+  link && link.classList.add("action");
+  if (ref.nodeName === "TEMPLATE") {
+    const clon = ref.content.cloneNode(true);
+    ref.parentNode.insertBefore(clon, ref);
+    route.loaded = true;
+    ref.parentNode.removeChild(ref);
+    const _ref = document.querySelector(`[route-id="${route.component}"]`);
+    animate(_ref);
+  } else {
+    ref.style.display = "";
+    animate(ref);
   }
 }
 
 window.addEventListener("hashchange", () => {
-  hashchange(location.hash.substr(1));
+  hashchange(location.hash.substr(1) || "home");
 });
 
+window.onload = () => {
+  const _router = router.filter(r => !r.loaded)
+  for (let index = 0; index < _router.length; index++) {
+    const route = _router[index];
+    const ref = document.querySelector(`[route-id="${route.component}"]`);
+    if (ref.nodeName === "TEMPLATE") {
+      const clon = ref.content.cloneNode(true);
+      ref.parentNode.insertBefore(clon, ref);
+      route.loaded = true;
+      ref.parentNode.removeChild(ref);
+      const _ref = document.querySelector(`[route-id="${route.component}"]`);
+      _ref.style.display = "none";
+    }
+  }
+}
+
 import "./home";
+import "./product";
+import "./store";
+import "./join";
